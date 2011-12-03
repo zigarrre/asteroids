7a0bae1d082e81a477b2cac4406b8e461b2f1941
zigarrre <zigarrre@gmail.com>
2011-12-03 21:34:22 +0100

    Added implementation file and class header for class Game

---
+++ include/Game.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "include/Gamestate.hpp"
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class Game {
+    public:
+        Game(sf::RenderTarget& renderTarget);
+        startGameLoop();
+    protected:
+    private:
+        sf::RenderTarget& renderTarget;
+        vector<Gamestate> gamestates;
+        int activeState;
+};
+
+#endif // GAME_HPP
---
+++ src/Game.cpp
@@ -0,0 +1,6 @@
+#include "Game.hpp"
+
+Game::Game()
+{
+
+}
