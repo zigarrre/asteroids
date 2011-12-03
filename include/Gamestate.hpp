34023759e7ecfe44ca42ced14832fd144dfd8940
zigarrre <zigarrre@gmail.com>
2011-12-03 21:17:44 +0100

    added abstract class Gamestate

---
+++ include/Gamestate.hpp
@@ -0,0 +1,16 @@
+#ifndef GAMESTATE_HPP
+#define GAMESTATE_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Gamestate {
+    public:
+        Gamestate() {}
+        virtual ~Gamestate() {}
+        virtual void update(float deltaTime) = 0;
+        virtual void draw(sf::RenderTarget& renderTarget) = 0;
+    protected:
+    private:
+};
+
+#endif // GAMESTATE_HPP
