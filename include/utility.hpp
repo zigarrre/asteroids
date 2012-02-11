6ae8d9f3534ffe0808bf498180e77760f9ffd4ec
zigarrre <zigarrre@gmail.com>
2012-02-11 17:45:05 +0100

    added toString function to utility.hpp

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
+#include <sstream>
+
 const float PI = 3.1415926f;
+
+namespace rrr {
+
+	template <class T>
+	std::string toString (const T& t) {
+		std::stringstream ss;
+		ss << t;
+		return ss.str();
+	}
+
+}
