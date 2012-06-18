4ec155279da323a97518bd4cb8acc459fe10312d
zigarrre <zigarrre@gmail.com>
2012-06-18 21:20:53 +0200

    fixed some bugs in Highscore and added licens header

--- include/Highscore.hpp
+++ include/Highscore.hpp
@@ -1,3 +1,9 @@
+/**
+* Copyright (c) 2012, Jens Mölzer
+* This file is part of the asteroids project and is realeased under the BSD 2-Clause License.
+* For more Details on licensing, see 'LICENCE.txt'.
+**/
+
 #pragma once
 
 #include <string>
@@ -7,8 +13,8 @@
 **/
 class HighscoreEntry {
 public:
-	unsigned int score;
-	std::string name;
+    unsigned int score;
+    std::string name;
 };
 
 /**
@@ -18,47 +24,47 @@ public:
 class Highscore {
 
 public:
-	Highscore();
-	~Highscore();
+    Highscore();
+    ~Highscore();
 
-	/**
-	* Loads the scores from the specified file. If file can't be read false is returned and a new empty higscore table initialisied.
-	* If everythinh is OK fals is returned.
-	**/
-	bool loadFromFile(const std::string& path);
+    /**
+    * Loads the scores from the specified file. If file can't be read false is returned and a new empty higscore table initialisied.
+    * If everythinh is OK fals is returned.
+    **/
+    bool loadFromFile(const std::string& path);
 
-	/**
-	* Saves the scores to the specified file. If an error occures false is returned else true.
-	**/
-	bool saveToFile(const std::string& path) const;
+    /**
+    * Saves the scores to the specified file. If an error occures false is returned else true.
+    **/
+    bool saveToFile(const std::string& path) const;
 
-	unsigned int getCurrentScore() const { return currentScore; }
-	
-	void resetCurrentScore() { currentScore = 0; }
+    unsigned int getCurrentScore() const { return currentScore; }
+    
+    void resetCurrentScore() { currentScore = 0; }
 
-	Highscore& operator+=(unsigned int a) { currentScore += a; }
+    unsigned int operator+=(unsigned int a) { currentScore += a; return getCurrentScore(); }
 
-	/**
-	* Returnes the Score object for the specified place.
-	* Counting starts with zero.
-	* Only values from 0-9 are allowed (only the 10 best scores are saved).
-	**/
-	const HighscoreEntry& operator[](unsigned int index) const;
+    /**
+    * Returnes the Score object for the specified place.
+    * Counting starts with zero.
+    * Only values from 0-9 are allowed (only the 10 best scores are saved).
+    **/
+    const HighscoreEntry& operator[](unsigned int index) const;
 
-	/**
-	* Returnes true if the current score is a new highscore.
-	**/
-	bool newHighscore() const;
+    /**
+    * Returnes true if the current score is a new highscore.
+    **/
+    bool newHighscore() const;
 
-	/**
-	* Save current score as highscore. If the current score is worse than the worst highscore it is not saved.
-	* If the current score is better than the worst highscore and saved true is returned.
-	* The scores are NOT writen to disk. This function modifies only the RAM call saveToFile afterwards to save scores to disk.
-	**/
-	bool saveCurrentScore(const std::string& name);
+    /**
+    * Save current score as highscore. If the current score is worse than the worst highscore it is not saved.
+    * If the current score is better than the worst highscore and saved true is returned.
+    * The scores are NOT writen to disk. This function modifies only the RAM call saveToFile afterwards to save scores to disk.
+    **/
+    bool saveCurrentScore(const std::string& name);
 
 private:
-	unsigned int currentScore;
-	HighscoreEntry* scores;
+    unsigned int currentScore;
+    HighscoreEntry* scores;
 
 };
--- src/Highscore.cpp
+++ src/Highscore.cpp
@@ -1,3 +1,9 @@
+/**
+* Copyright (c) 2012, Jens Mölzer
+* This file is part of the asteroids project and is realeased under the BSD 2-Clause License.
+* For more Details on licensing, see 'LICENCE.txt'.
+**/
+
 #include "Highscore.hpp"
 #include "utility.hpp"
 #include <fstream>
@@ -9,78 +15,84 @@ class BadIndexException : public exception {
 };
 
 Highscore::Highscore() :
-	currentScore(0),
-	scores(new HighscoreEntry[9])
+    currentScore(0),
+    scores(new HighscoreEntry[9])
 {
-	for(unsigned int i = 0; i < 10; ++i) {
-		scores[i].name = "-";
-		scores[i].score = 0;
-	}
+    for(unsigned int i = 0; i < 9; ++i) {
+        scores[i].name = "-";
+        scores[i].score = 0;
+    }
 }
 
 Highscore::~Highscore() {
-	delete[] scores;
+    delete[] scores;
 }
 
 bool Highscore::loadFromFile(const std::string& path) {
-	ifstream file;
-	file.open(path);
+    ifstream file;
+    file.open(path);
 
-	if(!file.good())
-		return false;
+    if(!file.good())
+        return false;
 
-	for(unsigned int i = 0; i < 10 && file.good(); ++i) {
-		string line;
-		getline(file, line);
-		istringstream iss;
-		iss.str(line);
-		getline(iss, scores[i].name, ';');
-		string score;
-		getline(iss, score, ';');
-		scores[i].score = rrr::fromString<unsigned int>(score);
-	}
+    for(unsigned int i = 0; i < 10 && file.good(); ++i) {
+        string line;
+        getline(file, line);
+        istringstream iss;
+        iss.str(line);
+        getline(iss, scores[i].name, ';');
+        string score;
+        getline(iss, score, ';');
+        scores[i].score = rrr::fromString<unsigned int>(score);
+    }
 
-	file.close();
+    file.close();
+
+    return true;
 }
 
 bool Highscore::saveToFile(const std::string& path) const {
-	ofstream file;
-	file.open(path);
+    ofstream file;
+    file.open(path);
 
-	if(!file.good())
-		return false;
+    if(!file.good())
+        return false;
 
-	for(unsigned int i = 0; i < 10; ++i) {
-		file<<scores[i].name<<';'<<scores[i].score<<'\n';
-	}
+    for(unsigned int i = 0; i < 10; ++i) {
+        file<<scores[i].name<<';'<<scores[i].score<<'\n';
+    }
+
+    return true;
 }
 
 const HighscoreEntry& Highscore::operator[](unsigned int index) const {
-	if(index > 9)
-		throw(BadIndexException());
-	else
-		return scores[index];
+    if(index > 9)
+        throw(BadIndexException());
+    else
+        return scores[index];
 }
 
 bool Highscore::newHighscore() const{
-	if(currentScore > scores[9].score)
-		return true;
-	else
-		return false;
+    if(currentScore > scores[9].score)
+        return true;
+    else
+        return false;
 }
 
 bool Highscore::saveCurrentScore(const std::string& name) {
-	unsigned int pos = 10;
-	while(currentScore > scores[pos-1].score && pos > 0) // find out where to insert new score
-		--pos;
-	
-	if(pos > 9) // no new highscore
-		return false;
+    unsigned int pos = 10;
+    while(currentScore > scores[pos-1].score && pos > 0) // find out where to insert new score
+        --pos;
+    
+    if(pos > 9) // no new highscore
+        return false;
 
-	for(unsigned int i = 9; i > pos; --i) // move older scores one back
-		scores[i] = scores[i-1];
+    for(unsigned int i = 9; i > pos; --i) // move older scores one back
+        scores[i] = scores[i-1];
 
-	// insert new score
-	scores[pos].name = name;
-	scores[pos].score = currentScore;
+    // insert new score
+    scores[pos].name = name;
+    scores[pos].score = currentScore;
+
+    return true;
 }
