18427f602fc11c2ff7cba0b99431214a43f15778
zigarrre <zigarrre@gmail.com>
2013-07-04 16:50:40 +0200

    documented ButtonManager

--- include/ButtonManager.hpp
+++ include/ButtonManager.hpp
@@ -15,11 +15,42 @@ class ButtonManager {
 public:
 
     ButtonManager(const sf::Vector2f& pos, sf::RenderWindow& renderWindow);
+    
+    /**
+    * Update the ButtonManager, should be called every frame
+    **/
     void update(float deltaTime);
+    
+    /**
+    * Draw all buttons to the RenderWindow which was given in the constructor
+    **/
     void draw();
+    
+    /**
+    * Add a new button to the manager.
+    *
+    * Use this function if you don't need control over the button.
+    **/
     void addButton(const std::string& text, boost::function<void ()> onClickCallback);
+    
+    /**
+    * Add an existing button to the manager.
+    *
+    * Use this function of you need future control over the button
+    *
+    * Note: It is not recommended to call setPosition() or setActive() onto the button,
+    * all other functions are okay.
+    **/
     void addButton(std::shared_ptr<ManagedButton> button);
+    
+    /**
+    * Remove a button from the manager
+    **/
     void removeButton(size_t index);
+    
+    /**
+    * Change the layout of the manager, possible layouts are specified as constants in this class
+    **/
     void setLayout(unsigned int layout);
     
     enum { VERTICALLY = 0, HORIZONTALLY };
